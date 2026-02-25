c0a5d445b3659b7230f2b3e7cf6098be7d48f674
aditi2605 <aditi.parikh0526@gmail.com>
2026-02-25 13:48:54 +0000

    fix: add Vercel URL to CORS allowed origins

--- backend/main.py
+++ backend/main.py
@@ -31,7 +31,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://habitual-pi.vercel.app/"],  
+    allow_origins=["http://localhost:5173", "http://localhost:3000", "https://habitual-pi.vercel.app", "habitual-aditi2605s-projects.vercel.app"],  
     allow_credentials=True,
     allow_methods=["*"], 
     allow_headers=["*"], 
